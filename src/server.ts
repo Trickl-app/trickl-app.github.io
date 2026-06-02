4f1aaad4b150a906198b0845c2682da6f34ca4f0
jonnorrie <jonathontirabassi@gmail.com>
2026-06-02 13:34:17 -0400

    Initialized React app with Vite and TypeScript
    Co-authored-by: lewisian8787 <lewisian8787@gmail.com>
    Co-authored-by: Nazeer Shaikh <63686800+nash226@users.noreply.github.com>
    Co-authored-by: Syed <107073836+5y3d@users.noreply.github.com>
    Co-authored-by: Jonathon Norrie <122530275+jonnorrie@users.noreply.github.com>

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,22 @@
 import express from "express";
+import path from "path";
 
-const app=express();
-const PORT = 4567
+const app = express();
+const PORT = 4567;
 
+const clientPath = path.resolve(process.cwd(), "client", "dist");
+
+app.use(express.static(clientPath));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send()
-})
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(clientPath, "index.html"));
+});
 
 app.get("/health", (req, res) => {
-  res.json({ status: "Healthy" })
-})
+  res.json({ status: "Healthy" });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
